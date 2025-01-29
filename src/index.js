8dd7dbfb955d9820de9108ee6b29658e248c6d81
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2025-01-29 14:32:00 +0530

    CreateRoot integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot
+import { createRoot } from 'react-dom/client'; 
 import App from './App';
-
-// Find the root element in your HTML
 const container = document.getElementById('root');
-
-// Create a root
 const root = createRoot(container);
-
-// Render your app
 root.render(<App />);
