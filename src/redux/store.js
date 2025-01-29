669e4bd9a6e258e4893bad0e23c37a1b527bced3
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2025-01-29 14:31:00 +0530

    CreateStore to configure Store to be in handy with redux@latest

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,7 @@ import rootReducer from './reducers/rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  // Optional configuration options:
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  // devTools: process.env.NODE_ENV !== 'production',
-  // preloadedState: {},
-  // enhancers: [],
+  
 });
 
 export default store;
